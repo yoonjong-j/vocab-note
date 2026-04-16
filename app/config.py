2b0b7ed490900c744c5f441bcefcae475c979d46
yoonjong-j <yoonjong.jang.dev@gmail.com>
2026-04-16 20:44:06 +0900

    chore: adjust comments' locations

--- app/config.py
+++ app/config.py
@@ -2,11 +2,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 # Settings class to manage environment variables and configurations
 class Settings(BaseSettings):
-    # Variable to store the PostgreSQL connection string from .env
-    database_url: str
-
-    # Configuration to tell Pydantic to read from the ".env" file
-    model_config = SettingsConfigDict(env_file=".env")
+    database_url: str                                  # Variable to store the PostgreSQL connection string from .env
+    model_config = SettingsConfigDict(env_file=".env") # Configuration to tell Pydantic to read from the ".env" file
 
 # Create a global instance of settings to be used throughout the application
 settings = Settings()
